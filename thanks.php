e62495ca0e233f429cc6ae56da742d5d4c5b52c5
Jesse McClure <admin@darwinsdogs.org>
2016-08-25 14:03:48 -0400

    thanks page styling

--- thanks.php
+++ thanks.php
@@ -1,13 +1,12 @@
 <?php if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) die(); ?>
-<div class="nav_target" id="thanks_page">
-<div id="thanks">
+<div class="nav_target" id="thanks">
 
 <h3>Thank You!</h3>
 
 <?php echo $user['first'], ' and ', $dog['name']; ?><br/>
 
-<div class="avatar" id="user_avatar" style="background-image: url(<?php echo $dd_root . 'res/users/' . $user['id'] . '.png' . $post_img; ?>);"></div>
-<div class="avatar" id="dog_avatar" style="background-image: url(<?php echo $dd_root . 'res/dogs/' . $dog['id'] . '.png' . $post_img; ?>);"></div>
+<div class="avatar" id="user_avatar" style="background-image: url(<?php echo $dd_root . 'res/users/' . $user['image'] . '.png' . $post_img; ?>);"></div>
+<div class="avatar" id="dog_avatar" style="background-image: url(<?php echo $dd_root . 'res/dogs/' . $dog['image'] . '.png' . $post_img; ?>);"></div>
 <br/>
 
 <span>for your contribution<br/>to genetics research!</span><br/>
@@ -16,6 +15,5 @@
 
 <span style="font-size: 50%;">Back Home</span>
 
-</div>
 </div>
 
