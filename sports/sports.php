70d46fd65850d3402a39d180d42719e5f1bf0955
Jesse McClure <admin@darwinsdogs.org>
2016-11-16 11:55:29 -0500

    fixed sports subpage links that dropped dog id

--- sports/sports.php
+++ sports/sports.php
@@ -5,7 +5,7 @@ foreach($rec as $s => $val) {
 	else $val = round($val * $mult);
 	$stars = '<div class="paws"><div class="pawback"></div><div class="pawfront" style="width: ' . $val . '%;" ></div></div>';
 	$s++;
-	echo '<a title="', $title[$s], '" href="?pg=sports&arg=', $s, '">',
+	echo '<a title="', $title[$s], '" href="?pg=sports&arg=', $s, '&id=', $dog['id'], '">',
 		'<div class="sport"><div class="img" style="background-image: url(', $dd_surveys, '/res/ui/sports', $s, '.jpg);"></div>',
 		'<div class="name" style="background: rgba(', $color[$s], ',1);">', $name[$s], $stars, '</div></div></a>', PHP_EOL;
 }
