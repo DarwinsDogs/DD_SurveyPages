9040cabb8c64334b061f883f46448adc5ab76436
Jesse McClure <admin@darwinsdogs.org>
2016-11-23 12:07:36 -0500

    pie chars are working!

--- review.php
+++ review.php
@@ -13,8 +13,6 @@ $colors = Array(
 	Array( '1,102,94', '53,151,143', '128,205,193', '199,234,229', '220,220,220', '246,232,195', '223,194,125', '191,129,45', '140,81,10' )
 );
 
-
-
 /* get overview */
 $stmt = $db->prepare('SELECT title FROM surveys WHERE id = :sn');
 $stmt->bindValue(':sn', $npage, PDO::PARAM_INT);
@@ -58,6 +56,7 @@ function review_likert($q) {
 	}
 }
 
+/*
 function review_choices($q) {
 	global $db, $user, $colors, $dogs;
 	$stmt = $db->prepare('SELECT answer, COUNT(answer) AS count, GROUP_CONCAT( CASE WHEN dog IN ( SELECT id FROM dogs WHERE owner = :uid )
@@ -76,6 +75,41 @@ function review_choices($q) {
 			'<div style="border: 1px solid black; position: absolute; top: 0px; left: 0px; width: ', $w, '%; background: rgba(', $colors[count($opts)][$r['answer']], ',1); color: white; overflow: hidden; white-space: nowrap;">&nbsp;',
 			$opts[$r['answer']], ' (', $r['count'], ')</div></div>';
 	}
+}
+*/
+
+function prefix($attr, $val) { foreach (array('-moz-','-ms-','-o-','-webkit-') as $pre) echo $pre, $attr, ': ', $val, '; '; echo $attr, ': ', $val; }
+function review_choices($q) {
+	global $db, $user, $colors, $dogs;
+	$stmt = $db->prepare('SELECT answer, COUNT(answer) AS count, GROUP_CONCAT( CASE WHEN dog IN ( SELECT id FROM dogs WHERE owner = :uid )
+		THEN ( SELECT name FROM dogs WHERE id = dog ) ELSE NULL END SEPARATOR ", " ) AS dogs FROM answers WHERE question = :qn AND answer != "" GROUP BY answer ORDER BY answer');
+	$stmt->bindValue(':uid', $user['id'], PDO::PARAM_INT);
+	$stmt->bindValue(':qn', $q['id'], PDO::PARAM_INT);
+	if (!$stmt->execute()) die('Query error: ' . $stmt->errorInfo());
+	$review = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$sum = 0;
+
+	$vals = Array();
+	foreach ($review as $r) $vals[] = $r['count'];
+	$opts = explode('|', pronouns($q['options']));
+
+	$n = count($vals); $degrees[0] = 0;
+	$col = $colors[$n];
+	for ($i = 1; $i < $n; $i++) $vals[$i] += $vals[$i-1];
+	for ($i = 1; $i < $n; $i++) $degrees[$i] = $vals[$i-1] * 360 / $vals[$n-1];
+	for ($n = 0; $n < count($degrees) && $degrees[$n] < 180; $n++);
+	echo '<div class="pie_half pie_left" style="background: rgba(', $col[$n-1], ',1);">', PHP_EOL;
+	for ($n = 0; $n < count($degrees); $n++)
+		echo "\t", '<div class="pie_slice" style="background: rgba(', $col[$n], ',1); ', prefix('transform', 'rotate(' . $degrees[$n] . 'deg)'), '"></div>', PHP_EOL;
+	echo '</div><div class="pie_half pie_right">', PHP_EOL;
+	for ($n = 0; $n < count($degrees) && $degrees[$n] < 180; $n++)
+		echo "\t", '<div class="pie_slice" style="background: rgba(', $col[$n], ',1); ', prefix('transform', 'rotate(' . $degrees[$n] . 'deg)'), '"></div>', PHP_EOL;
+	echo '</div>', PHP_EOL, '<div class="pie_labels">', PHP_EOL;
+	for ($n = 0; $n < count($opts) ; $n++)
+		echo "\t", '<div class="pie_label"><div style="background: rgba(', $col[$n], ',1);"></div><span>', ucfirst($opts[$n]), '</span><span class="num">(', $vals[$n], 
+			($review[$n]['dogs'] ? ' including ' . $review[$n]['dogs'] : '' ), ')</span></div>', PHP_EOL;
+	echo '&nbsp;</div>', PHP_EOL;
+
 }
 
 function review_multinumeric($q) {
@@ -114,6 +148,8 @@ function review_multinumeric($q) {
 		echo '<tr><td>', ($i + 1), '</td><td>', $opts[$i], '</td></tr>', PHP_EOL;
 	echo '</tbody></table>', PHP_EOL;
 }
+
+
 
 
 echo '<h3>Review for "', $title, '"</h3>', PHP_EOL,
